b37f45fecb18a1359c2f9d77f6809470aded1aac
13812838001 <13812838001@163.com>
2024-01-29 15:12:47 +0800

    排除

---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <a-menu mode="horizontal" :default-selected-keys="['1']">
+            <a-menu-item key="0" disabled> </a-menu-item>
+            <a-menu-item key="1">Home</a-menu-item>
+            <a-menu-item key="2">Solution</a-menu-item>
+            <a-menu-item key="3">Cloud Service</a-menu-item>
+            <a-menu-item key="4">Cooperation</a-menu-item>
+        </a-menu>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped></style>
